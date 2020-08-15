92dc44e4e0a406a340948c2f1dd3d0b19ef7517b
Aaron Schachter <aaronschachter@gmail.com>
2020-08-14 17:59:41 -0700

    Cleanup

--- api/middleware/parseFlowEvent.js
+++ api/middleware/parseFlowEvent.js
@@ -12,23 +12,26 @@ module.exports = function parseFlowEvent() {
       logger.debug('Received TextIt flow event', { body });
 
       const { contact, flow, results } = req.body;
+      const { uuid } = contact;
 
-      const contactId = contact.uuid;
-
-      contact.url = textIt.getUrlForContactId(contactId);
-      // Removes +tel prefix
-      contact.phone = contact.urn.substring(5);
-
-      const textItRes = await textIt.getContactById(contactId);
+      const textItRes = await textIt.getContactById(uuid);
 
       logger.debug('TextIt response', textItRes);
 
       const { fields, groups, name, blocked, stopped, created_on, modified_on } = textItRes;
 
       req.data = {
-        contact,
+        uuid,
         flow,
         results,
+        phone: contact.urn.substring(5),
+        name,
+        // Removes +tel prefix.
+        url: textIt.getUrlForContactId(uuid),
+        blocked,
+        stopped,
+        created_on,
+        modified_on,
         fields,
         groups: groups.map(group => group.name).join(', '),
       };
