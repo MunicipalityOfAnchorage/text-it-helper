580b598be1f9c15ce5d347aab9aba63bd77bca53
Aaron Schachter <aaronschachter@gmail.com>
2020-08-15 16:55:42 -0700

    Remove unused auth config

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  apiKey: process.env.AUTH_API_KEY,
-  unauthorizedErrorMessage: 'Invalid or missing auth parameters. Unauthorized.',
-  unauthorizedErrorCode: 401,
-};
