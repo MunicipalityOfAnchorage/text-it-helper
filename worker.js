fb0b63a034449f273c80a95b478063fa132e4090
Aaron Schachter <aaronschachter@gmail.com>
2020-08-17 07:04:05 -0700

    Adds testModeEnabled

--- worker.js
+++ worker.js
@@ -9,10 +9,16 @@ const batches = require('./lib/batches');
 // Places new subscribers into a batch group, if they haven't been added to one yet.
 const main = async () => {
   const newSubscribers = await batches.getNewSubscribers();
+
+  logger.debug('newSubscribers', newSubscribers);
+
   const lastBatchGroup = await batches.getLastBatchGroup();
+
+  logger.debug('lastBatchGroup', lastBatchGroup);
+
   const result = await batches.addContactsToBatchGroup(newSubscribers, lastBatchGroup);
 
-  logger.info('batch group results', { result });
+  logger.info(result ? 'updated batch groups' : 'no batch group updates', { result });
 };
 
 (async () => {
