b0323bbaff9c087a93ec59e3b085a8cced993f42
Aaron Schachter <aaronschachter@gmail.com>
2020-09-05 19:34:50 -0700

    Starts on digest worker

--- worker.js
+++ worker.js
@@ -2,13 +2,22 @@
 
 require('dotenv').config();
 
+const { argv } = require('yargs')
 const logger = require('heroku-logger');
 
 const batches = require('./lib/batches');
+const digest = require('./lib/tasks/digest');
 
 // Places new subscribers into a batch group, if they haven't been added to one yet.
 const main = async () => {
   try {
+    if (argv.task === 'digest') {
+      const result = await digest.send();
+
+      console.log({ result }); 
+      return;
+    }
+
     const newSubscribers = await batches.getNewSubscribers();
     logger.debug('newSubscribers', newSubscribers);
 
