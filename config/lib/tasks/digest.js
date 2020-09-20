18d3eac09d23d37757f7d17a5a0d12bfc55cc737
Aaron Schachter <aaronschachter@gmail.com>
2020-09-19 20:32:37 -0700

    Starts on created/updated

--- config/lib/tasks/digest.js
+++ config/lib/tasks/digest.js
@@ -2,5 +2,7 @@
 
 module.exports = {
   airtableTableName: process.env.DIGEST_AIRTABLE_TABLE_NAME,
+  textItFlowId: process.env.DIGEST_TEXT_IT_FLOW_ID,
+  textItFlowValueNames: process.env.DIGEST_TEXT_IT_FLOW_VALUE_NAMES.split(','),
   zapierWebhook: process.env.DIGEST_ZAPIER_WEBHOOK,
 };
